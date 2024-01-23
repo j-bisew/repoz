f715bd0f80510e240dc4e5b5ac1e51ba8a78a282
j-bisew <148365049+j-bisew@users.noreply.github.com>
2024-01-23 21:09:53 +0100

    Update kod.py

--- kod.py
+++ kod.py
@@ -1,4 +1,4 @@
-import pytest
+import unittest
 
 def NWD(a, b):
     if a == b:
@@ -7,6 +7,16 @@ def NWD(a, b):
         return NWD(a - b, b)
     else:
         return NWD(a, b - a)
+        
+class TestNWDFunction(unittest.TestCase):
 
-def test_NWD():
-    assert NWD(4,2) == 2
+    def test_same_numbers(self):
+        result = NWD(5, 5)
+        self.assertEqual(result, 5)
+
+    def test_a_greater_than_b(self):
+        result = NWD(12, 8)
+        self.assertEqual(result, 4)
+
+if __name__ == '__main__':
+    unittest.main()
